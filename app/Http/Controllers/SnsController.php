7f75976a34b6352292a196365ba7e8c2629e835e
CMHayden <haydencallum4@gmail.com>
2019-06-06 23:44:53 +0200

    add controller for post route

--- app/Http/Controllers/SnsController.php
+++ app/Http/Controllers/SnsController.php
@@ -3,8 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use AWS;
 
 class SnsController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+
+        $json_data = $request->getContent();
+
+        $aws = AWS::createClient('sns');
+
+        $aws->publish([
+            'Message' => $json_data,
+            'TargetArn' => 'arn:aws:sns:eu-west-1:310005059065:sns_new_sale'
+        ]);
+
+        return 'Success';
+    }
 }
