01ee029336d705af2244dc4d58f26a960ccb49ab
CMHayden <haydencallum4@gmail.com>
2019-06-07 13:50:05 +0200

    update path for post request to sns

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('send', 'SnsController@store');
+Route::post('aws/sns/snsProjectTestTopic', 'SnsController@store');
